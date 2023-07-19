e6b0c152a9b896a021a4179cbc80845aa8a900bf
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-07-19 20:29:33 +0100

    styling input bar, user-container

--- promage-dashboard/src/Dashboard-Components/Header.jsx
+++ promage-dashboard/src/Dashboard-Components/Header.jsx
@@ -6,11 +6,20 @@ function Header() {
     <div className='header-container'>
       <h1 className='dashboard'>Dashboard</h1>
       <div className='user-wrapper'>
-        <input className='search-input' placeholder='Search' />
-        <div className='bio-wrapper'>
-          <div>Picture</div>
-          <div>Name</div>
-          <div>aroow down</div>
+        <input className='search-input' placeholder='Search for anything...' />
+
+        <div className='wrapper'>
+          <div className='circle'>.</div>
+          <div className='bio-wrapper'>
+            <div className='picture'>.</div>
+            <div className='name'>
+              <h3>Vibe Coder</h3>
+              <p>Frontend Developer</p>
+            </div>
+            <div className=''>
+              <i className="las la-angle-down"></i>
+            </div>
+          </div>
         </div>
       </div>
     </div>
