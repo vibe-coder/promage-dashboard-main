349d049825bcc23bf44e70cc143c2d5868cf041e
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-07-20 11:21:59 +0100

    styling overview section

--- promage-dashboard/src/Dashboard-Components/Overview.jsx
+++ promage-dashboard/src/Dashboard-Components/Overview.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./SCSS/Overview.scss"
+import overviewIcon from "../Images/overview-icon.svg"
+import increaseIcon from "../Images/increase-icon.svg"
 
 function Overview() {
   return (
     <div className='overview-container'>
-      <div className='income-icon'>Image</div>
-      <div className='income-figure'>Texts</div>
+      <div className='income-icon'>
+        <img src={overviewIcon} alt='overview-icon' />
+      </div>
+      <div className='income-figure'>
+        <p>Total Revenue</p>
+        <h1>$53,00989</h1>
+        <div className='increase-wrapper'>
+          <img src={increaseIcon} alt='increase-icon' />
+          <p>12% increase from last month</p>
+        </div>
+      </div>
     </div>
   )
 }
