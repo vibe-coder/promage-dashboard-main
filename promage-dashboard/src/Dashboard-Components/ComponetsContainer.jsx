9700468d75f66e1ee6fde168ebfa186ab7a2fcbd
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-08-28 10:45:46 +0100

    reformatted code to useCollapsed

--- promage-dashboard/src/Dashboard-Components/ComponetsContainer.jsx
+++ promage-dashboard/src/Dashboard-Components/ComponetsContainer.jsx
@@ -7,27 +7,47 @@ import Task from './Task'
 import Sidebar from './Sidebar'
 import arrowDown from "../Images/arrow-down.svg"
 import { useState } from 'react'
+import { useCollapse } from 'react-collapsed'
 
 function ComponetsContainer() {
 
-  const [sideBarWidth, setSideBarWidth] = useState(false)
+  // const [sideBarWidth, setSideBarWidth] = useState(false)
 
   // let sideBar = document.getElementById("sidebar-wrapper")
+
+  const [isExpanded, setExpanded] = useState(false)
+  const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded })
   
 
 
 
   return (
     <div className='component-container'>
+
       <div className='sidebar-component'>
-        {sideBarWidth && <div id='sidebar-box' className='sidebar-box'>
+
+      <section className='sidebar-box' {...getCollapseProps()}>
           <Sidebar/>
-        </div>}
+        </section>
         
-        <button onClick={() => setSideBarWidth(!sideBarWidth)} className="reveal-button">
+        <button     {...getToggleProps({
+            onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+          })}  className='reveal-button'>
+          {isExpanded ? '<' : '>'}
+        </button>
+
+
+      </div>
+      
+      {/* <div className='sidebar-component'>
+       <div className='sidebar-box'>
+          <Sidebar/>
+        </div>
+        
+        <button className="reveal-button">
           <img src={arrowDown} alt='arrow down'/>
         </button>
-      </div>  
+      </div>   */}
 
 
       <div className='other-components'>
