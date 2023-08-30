f1c5520b4fcf45c0dc4a1f2b92e975254ddd4f8b
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-08-30 15:00:57 +0100

    Created Project Summary Data

---
+++ promage-dashboard/src/Dashboard-Components/ProjectSummaryData.js
@@ -0,0 +1,41 @@
+const SummaryData = [
+  {
+    name: "Nelsa Web Development",
+    manager: "Om Prakash Sao",
+    date: "May 25, 2023",
+    status: "Completed",
+    progress: "100%"
+  },
+  {
+    name: "Datascale AI App",
+    manager: "Neilsan Mando",
+    date: "Jun 20, 2023",
+    status: "Delayed",
+    progress: "35%"
+  },
+  {
+    name: "Media Channel Branding",
+    manager: "Tiruvelly Priya",
+    date: "July 13, 2023",
+    status: "At risk",
+    progress: "68%"
+  },
+  {
+    name: "Corlax iOS App development",
+    manager: "Matt Hannery",
+    date: "Dec 20, 2023",
+    status: "Completed",
+    progress: "100%"
+  },
+
+  {
+    name: "Website Builder Development",
+    manager: "Sukumar Rao",
+    date: "Mar 15, 2024",
+    status: "On going",
+    progress: "50%"
+  }
+]
+
+
+export default SummaryData;
