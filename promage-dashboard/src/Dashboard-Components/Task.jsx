fefc0d89dc868d04cb928926717423f20ce437dc
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-01 22:39:54 +0100

    Created dropdown action for project workload btn

--- promage-dashboard/src/Dashboard-Components/Task.jsx
+++ promage-dashboard/src/Dashboard-Components/Task.jsx
@@ -2,8 +2,66 @@ import React from 'react'
 import "./SCSS/Task.scss"
 import arrowRight from "../Images/arrow-right-left.gif"
 import arrowDown from "../Images/arrow-down.svg"
+import { styled, alpha } from '@mui/material/styles';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import "./SCSS/Bio-Down-Button.scss"
+import Divider from '@mui/material/Divider';
+
+
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+
+
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 9,
+    marginTop: theme.spacing(1),
+    minWidth: 130,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[400],
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
 
 function Task() {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className='task-container'>
 
@@ -78,10 +136,32 @@ function Task() {
 
         <div className="project-workload-header">
           <h3>Projects Workload</h3>
-          <div>
-            <p>Last 3 Months</p>
-            <img src={arrowDown} alt="Arrow Down Icon"/>
-          </div>
+
+          <>
+            <div onClick={handleClick}>
+              <p>Last 3 Months</p>
+              <img src={arrowDown} alt="Arrow Down Icon"/>
+            </div>
+          
+            <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'demo-customized-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                className="styled-menu"
+              >
+                <MenuItem onClick={handleClose} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>Last 1 Month</p>
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>Last 2 Months</p>
+                </MenuItem>
+            </StyledMenu>
+          </>
         </div>
 
         {/* Projects Worload Body */}
