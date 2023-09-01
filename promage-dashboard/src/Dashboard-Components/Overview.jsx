0dc774aebee5c13628c705881229177fd327bbdc
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-01 10:37:39 +0100

    Created dropdown action for overview dropdown btn

--- promage-dashboard/src/Dashboard-Components/Overview.jsx
+++ promage-dashboard/src/Dashboard-Components/Overview.jsx
@@ -7,8 +7,74 @@ import projectsIcon from "../Images/projects-icon.svg"
 import timeIcon from "../Images/time-icon.svg"
 import resourcesIcon from "../Images/resources-icon.svg"
 import arrowDown from "../Images/arrow-down.svg"
+import { styled, alpha } from '@mui/material/styles';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import "./SCSS/Bio-Down-Button.scss"
+import user from "../Images/ME.jpg"
+import { useState } from 'react';
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import Divider from '@mui/material/Divider';
+
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+
+
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 9,
+    marginTop: theme.spacing(1),
+    minWidth: 130,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[400],
+    // boxShadow:
+    //   'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
 
 function Overview() {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  // const [checked, setChecked] = useState(false)  
+
   return (
 
     <div className='overview-container'>
@@ -17,11 +83,36 @@ function Overview() {
 
       <div className='overview-header'>
         <h3>Overview</h3>
-        <div className='date'>
+        <div onClick={handleClick} className='date'>
           <p>Last 30 days</p>
           <img src={arrowDown} alt='arrow down'/>
         </div>
+
+        <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        className="styled-menu"
+      >
+        <MenuItem onClick={handleClose} disableRipple>
+          <p style={{margin: 0, fontSize: '1rem'}}>Last 15 days</p>
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose} disableRipple>
+          <p style={{margin: 0, fontSize: '1rem'}}>Last 60 days</p>
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose} disableRipple>
+          <p style={{margin: 0, fontSize: '1rem'}}>Last 90 days </p>
+        </MenuItem>
+        </StyledMenu>
       </div>
+
+
 
       {/* Section Container */}
 
