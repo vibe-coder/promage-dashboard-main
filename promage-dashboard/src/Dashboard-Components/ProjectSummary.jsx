7c976ba4daccda3eccc448a7de717c0e2ef7e471
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-01 13:00:09 +0100

    Created a dropdown buutton for Project summary

--- promage-dashboard/src/Dashboard-Components/ProjectSummary.jsx
+++ promage-dashboard/src/Dashboard-Components/ProjectSummary.jsx
@@ -3,8 +3,87 @@ import "./SCSS/Project-Summary.scss"
 import arrowDown from "../Images/arrow-down.svg"
 import arrowRight from "../Images/arrow-right-left.gif"
 import progressMeasure from "../Images/progress-measure.svg"
+import { styled, alpha } from '@mui/material/styles';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import "./SCSS/Bio-Down-Button.scss"
+import Divider from '@mui/material/Divider';
+
+
+const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+
+
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 9,
+    marginTop: theme.spacing(1),
+    minWidth: 130,
+    color:
+      theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[400],
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
 
 function ProjectSummary() {
+
+
+  // Dropdown 1
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+   // Dropdown 2
+   const [anchorEl2, setAnchorEl2] = React.useState(null);
+   const open2 = Boolean(anchorEl2);
+   const handleClick2 = (event) => {
+     setAnchorEl2(event.currentTarget);
+   };
+   const handleClose2 = () => {
+     setAnchorEl2(null);
+   };
+       
+   // Dropdown 3
+    const [anchorEl3, setAnchorEl3] = React.useState(null);
+    const open3 = Boolean(anchorEl3);
+    const handleClick3 = (event) => {
+      setAnchorEl3(event.currentTarget);
+    };
+    const handleClose3 = () => {
+      setAnchorEl3(null);
+    };
+
   return (
     
     <div className='project-summary-container'>
@@ -27,20 +106,94 @@ function ProjectSummary() {
             <div className='title-wrapper'><h3>Project Summary</h3></div>
             
             <div className='button-wrapper'>  
-              <div className='selection'>
+            {/* Project */}
+              <>
+              <div onClick={handleClick} className='selection'>
                 <p>Project</p>
                 <img src={arrowDown} alt="Arrow Down Icon"/>
               </div>
+            
+                <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'demo-customized-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                className="styled-menu"
+              >
+                <MenuItem onClick={handleClose} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>Tasks</p>
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>Duties</p>
+                </MenuItem>
+                </StyledMenu>
+              </>
 
-              <div className='selection'>
+              {/* Project Manager */}
+              <>
+              <div onClick={handleClick2} className='selection'>
                 <p>Project Manager</p>
                 <img src={arrowDown} alt="Arrow Down Icon"/>
               </div>
 
-              <div className='selection'>
+                <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'demo-customized-button',
+                }}
+                anchorEl={anchorEl2}
+                open={open2}
+                onClose={handleClose2}
+                className="styled-menu"
+              >
+                <MenuItem onClick={handleClose2} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>Developer</p>
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose2} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>Designer</p>
+                </MenuItem>
+                </StyledMenu>
+              </>
+              
+              {/* Status */}
+              <>
+              <div onClick={handleClick3}  className='selection'>
                 <p>Status</p>
                 <img src={arrowDown} alt="Arrow Down Icon"/>
               </div>
+
+              <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                  'aria-labelledby': 'demo-customized-button',
+                }}
+                anchorEl={anchorEl3}
+                open={open3}
+                onClose={handleClose3}
+                className="styled-menu"
+              >
+                <MenuItem onClick={handleClose3} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>Completed</p>
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose3} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>Delayed</p>
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose3} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>At risk</p>
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose3} disableRipple>
+                  <p style={{margin: 0, fontSize: '1rem'}}>On going</p>
+                </MenuItem>
+                </StyledMenu>
+              </> 
             </div>
           </div>
 
