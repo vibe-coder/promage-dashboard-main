ab86a2fa7ada8b30a5fe72292bed6ea5b6025f2b
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-03 20:22:05 +0100

    Added id to Project Summary Data

--- promage-dashboard/src/Dashboard-Components/ProjectSummaryData.js
+++ promage-dashboard/src/Dashboard-Components/ProjectSummaryData.js
@@ -1,5 +1,6 @@
 const SummaryData = [
   {
+    id: 1,
     name: "Nelsa Web Development",
     manager: "Om Prakash Sao",
     date: "May 25, 2023",
@@ -7,6 +8,7 @@ const SummaryData = [
     progress: "100%"
   },
   {
+    id: 2,
     name: "Datascale AI App",
     manager: "Neilsan Mando",
     date: "Jun 20, 2023",
@@ -14,6 +16,7 @@ const SummaryData = [
     progress: "35%"
   },
   {
+    id: 3,
     name: "Media Channel Branding",
     manager: "Tiruvelly Priya",
     date: "July 13, 2023",
@@ -21,6 +24,7 @@ const SummaryData = [
     progress: "68%"
   },
   {
+    id: 4,
     name: "Corlax iOS App development",
     manager: "Matt Hannery",
     date: "Dec 20, 2023",
@@ -29,6 +33,7 @@ const SummaryData = [
   },
 
   {
+    id: 5,
     name: "Website Builder Development",
     manager: "Sukumar Rao",
     date: "Mar 15, 2024",
