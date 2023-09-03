68ab6116f7a3cf43b1e587d370e0e4fc6d376c34
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-03 20:09:55 +0100

    Created Today Task data

---
+++ promage-dashboard/src/Dashboard-Components/TodayTaskData.js
@@ -0,0 +1,21 @@
+const TaskData = [
+  {
+    task: "Create a user flow of social application design",
+    status: "Approved"
+  },
+  {
+    task: "Create a user flow of social application design",
+    status: "In review"
+  },
+  {
+    task: "Landing page design for Fintech project of Singapore",
+    status: "In review"
+  },
+  {
+    task: "Interactive prototype for app screens of deltamine project",
+    status: "Approved"
+  },
+]
+
+
+export default TaskData;
