2a02bbfa6a9957670d9ebcda4bdaa7cbfd00e6d6
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-09-03 21:02:13 +0100

    Added ID to Today task data

--- promage-dashboard/src/Dashboard-Components/TodayTaskData.js
+++ promage-dashboard/src/Dashboard-Components/TodayTaskData.js
@@ -1,17 +1,26 @@
 const TaskData = [
   {
+    id: 1,
     task: "Create a user flow of social application design",
     status: "Approved"
   },
   {
+    id: 2,
     task: "Create a user flow of social application design",
     status: "In review"
   },
   {
+    id: 3,
     task: "Landing page design for Fintech project of Singapore",
     status: "In review"
   },
   {
+    id: 4,
+    task: "Interactive prototype for app screens of deltamine project",
+    status: "On going"
+  },
+  {
+    id: 5,
     task: "Interactive prototype for app screens of deltamine project",
     status: "Approved"
   },
